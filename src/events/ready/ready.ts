ee990e191890b7cc5d4a6c304e88872b4b727dfb
PyPylia <me@pypylia.dev>
2025-10-20 17:36:51 +1100

    refactor: get rid of old TS-related files

--- src/events/ready/ready.ts
+++
@@ -1,6 +0,0 @@
-import type { Client } from "discord.js";
-import { logger } from "../../utils/logger.ts";
-
-export default function log(client: Client<true>) {
-    logger(`Successfully logged in`, "debug", client.user.tag);
-}
